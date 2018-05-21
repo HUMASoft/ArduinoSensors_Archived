c8bc4ae64b899117e7d60cfec57fcad087bd0702
mhlucho <mhlucho@hotmail.com>
2018-05-21 13:15:00 +0200

    Add files via upload

--- Serial/main.cpp
+++ Serial/main.cpp
@@ -1,13 +1,17 @@
 #include <QApplication>
 #include <QSerialPort>
 #include <QSerialPortInfo>
-#include <QMessageBox>
 #include <QDebug>
 QSerialPort *arduino;
 static const quint16 arduino_uno_vendor_id = 9025;
 static const quint16 arduino_MEGA = 66;
 QString arduino_port_name;
 bool arduino_is_available;
+QByteArray dataread;
+QString serialBuffer;
+QString serialBuffer1;
+QString data1;
+QString data2;
 
 int main(int argc, char *argv[])
 {
@@ -16,32 +20,35 @@ int main(int argc, char *argv[])
     arduino_port_name = "";
     arduino = new QSerialPort;
 
+
     //Parte # 2,buscar puertos con los identificadores de Arduino
     qDebug() << "Number of available ports: " << QSerialPortInfo::availablePorts().length();
     foreach(const QSerialPortInfo &serialPortInfo, QSerialPortInfo::availablePorts()){
         qDebug() << "Has vendor ID: " << serialPortInfo.hasVendorIdentifier();
-        if(serialPortInfo.hasVendorIdentifier()){
+        if(serialPortInfo.hasVendorIdentifier() && serialPortInfo.portName()=="ttyACM0"){
             qDebug() << "Port: " << serialPortInfo.portName();
+            qDebug() <<"\n";
             qDebug() << "Vendor ID: " << serialPortInfo.vendorIdentifier();
-        }
-        qDebug() << "Has Product ID: " << serialPortInfo.hasProductIdentifier();
-        if(serialPortInfo.hasProductIdentifier()){
+            qDebug() << "Has Product ID: " << serialPortInfo.hasProductIdentifier();
             qDebug() << "Product ID: " << serialPortInfo.productIdentifier();
          }
     }
+
 
     foreach(const QSerialPortInfo &serialPortInfo, QSerialPortInfo::availablePorts()){
         if(serialPortInfo.hasVendorIdentifier() && serialPortInfo.hasProductIdentifier()){
            if(serialPortInfo.vendorIdentifier() == arduino_uno_vendor_id){
              if(serialPortInfo.productIdentifier() == arduino_MEGA){
+               if(serialPortInfo.portName()=="ttyACM0") {
                 arduino_port_name=serialPortInfo.portName();
                 arduino_is_available = true;
+               }
              }
            }
         }
-    }
+     }
 
-    if(arduino_is_available){
+    if(arduino_is_available && arduino_port_name=="ttyACM0"){
         // open and configure the serialport
         arduino->setPortName(arduino_port_name);
         arduino->open(QIODevice::ReadWrite);
@@ -50,14 +57,24 @@ int main(int argc, char *argv[])
         arduino->setParity(QSerialPort::NoParity);
         arduino->setStopBits(QSerialPort::OneStop);
         arduino->setFlowControl(QSerialPort::NoFlowControl);
+        //QObject::connect(arduino, SIGNAL(readyRead()), this, SLOT(readSerial()));
     }
 
     while (arduino->waitForReadyRead(1000)){       /* Condition */
-         // arduino->write("\n");
         if(arduino->isReadable()){
-                 //qDebug() <<"Readable :"<<arduino->isReadable();
-                 QByteArray datoLeido = arduino->readLine();
-                 qDebug() <<"Dato :"<< datoLeido;
-        }
+                 dataread = arduino->readLine();
+                 if(dataread!="\n"){
+                 qDebug() <<"Dato total:"<<dataread;
+                 serialBuffer = QString::fromStdString(dataread.data());
+                 serialBuffer1 = QString::fromStdString(dataread.data());
+                 data1=serialBuffer1.remove(3,4);
+                 data2=serialBuffer.remove(0,3);
+                 data2=data2.remove(3,1);
+                 qDebug() <<"Dato 1:"<< data1;
+                 qDebug() <<"Dato 2:"<< data2;
+                 serialBuffer="";
+                 }
     }
-}
+   }
+ }
+
